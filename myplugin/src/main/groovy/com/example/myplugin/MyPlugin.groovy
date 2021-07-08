6ba991b4b985510395793cd2f88cd498e685b4e9
lidongjie <lidongjie2@yy.com>
2021-07-08 11:37:52 +0800

    添加自定义gradle插件

---
+++ myplugin/src/main/groovy/com/example/myplugin/MyPlugin.groovy
@@ -0,0 +1,16 @@
+package com.example.myplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Time:2021/7/7 20:23
+ * Author: lidongjie
+ * Description:
+ */
+class MyPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        println("this is MyPlugin")
+    }
+}
